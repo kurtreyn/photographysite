06008b7670bbce6fa872ffa2cf7d5f0b4672aa5c
kurtreyn <kurtreyn@yahoo.com>
2021-10-24 23:54:06 -0400

    added slider feature to gallery page, with  help from Charlie Thomason

--- script/mainScript.js
+++ script/mainScript.js
@@ -1,51 +1,76 @@
 'use strict';
 
 $(document).ready(function () {
+  //GET ALL IMAGES FROM GALLERY AND PLACE INTO ARRAY
+  const images = document.querySelectorAll('.gallery-img');
+  // CREATE VARIABLE TO STORE CURRENT IMG IN
+  let currentImg;
+
+  //LIGHTBOX TRIGGER TO OPEN
   $('.lightbox-trigger').click(function (e) {
-    //PREVENT DEFAULT ACTION OF GOING TO HYPERLINK
+    //PREVENT URL FROM POPPING UP
     e.preventDefault();
-
-    //GET CLICKED LINK'S HREF
-    var image_href = $(this).attr('href');
-
-    /*  
-   IF LIGHTBOX WINDOW HTML HAS ALREADY BEEN LOADED IN DOCUMENT, CHANGE IMG SRC TO MATCH HREF OF LINK THAT WAS CLICKED
-
-   IF LIGHTBOX WINDOW DOESN'T EXIST, CREATE AND INSERT IT (HAPPENS ON ITINIAL PAGE LOAD)
-   */
-
-    if ($('#lightbox').length > 0) {
-      // #lightbox EXISTS
-
-      //PLACES HREF AS IMG SRC VALUE
-      $('#content').html('<img src="' + image_href + '" />');
-
-      //DISPLAY LIGHTBOX WINDOW
-      $('#lightbox').show('fast');
-    } else {
-      //#lightbox DOESN'T EXIST, CREATE AND INSERT (RUNS ONCE ON INITIAL LOAD)
-
-      //CREATE HTML FOR LIGHTBOX
-      var lightbox =
-        '<div id="lightbox">' +
-        '<p class="closeBtn">&times;</p>' +
-        '<div id="content">' + //ADD CLICKED LINKS HREF TO SRC
-        '<img src="' +
-        image_href +
-        '" />' +
-        '</div>' +
-        '</div>';
-
-      //
-      $('body').append(lightbox);
-    }
+    //PUT BLURRY BACKGROUND WHEN LIGHTBOX IS ACTIVE
     $('.gallery-container').addClass('lightbox-active');
-  });
+    //SHOW LIGHTBOX
+    $('#lightbox').removeClass('lightbox-hidden');
+    //SET CURRENT IMAGE TO THE IMAGE THAT WAS CLICKED ON
+    currentImg = $(this).attr('href');
+    //DISPLAY THE IMAGE THAT WAS CLICKED ON IN THE LIGHTBOX
+    $('.lightbox-img').attr('src', currentImg);
+    //GIVE <a> THE CLASS OF CURRENT
+    $(this).addClass('current');
+  }); //END LIGHTBOX TRIGGER FUNCTION
 
-  //CLICK ON ANY PART OF PAGE TO REMOVE LIGHTBOX
-  $('body').on('click', '#lightbox', function () {
-    //USE 'ON' SINCE LIGHTBOX IS INSERTED INTO DOM
-    $('#lightbox').hide();
+  //CLOSE LIGHTBOX
+  $('.closeBtn').click(function () {
+    //REMOVE BLURRY BACKGROUND
     $('.gallery-container').removeClass('lightbox-active');
+    //HIDE THE LIGHTBOX
+    $('#lightbox').addClass('lightbox-hidden');
+    //REMOVE CURRENT CLASS FROM IMG
+    $('.current').removeClass('current');
+  }); //END CLOSE LIGHTBOX FUNCTION
+
+  //PREV/NEXT FUNCTION
+  $('.arrowBTN').click(function () {
+    //STORE ELEMENT THAT IS CURRENTLY USING THE CURRENT CLASS IN A VARIABLE
+    const current = $('.current');
+    // SET CURRENT IMG PULLED FROM ARRAY INTO A VARIABLE
+    const currentImgIndex = $(images).index(current);
+    //STORE NEXT BUTTON IN A VARIABLE
+    const isNextBtn = this.id === 'next';
+    //INDEX OF LAST IMG
+    const lastImgIndex = images.length - 1;
+    //CREATE UNDEFINED VARIABLE FOR FOR INDEX OF NEXT IMAGE TO SHOW
+    let nextImgIndex;
+
+    //NEXT BUTTON HANDLER
+    if (isNextBtn) {
+      // IF IT'S THE LAST IMAGE IN SET, GO TO FIRST IMG
+      if (currentImgIndex === lastImgIndex) {
+        nextImgIndex = 0;
+      } else {
+        // IF IT ISN'T THE LAST IMAGE IN SET, GO TO NEXT IMG
+        nextImgIndex = currentImgIndex + 1;
+      }
+      //PREV BUTTON HANDLER
+    } else {
+      if (currentImgIndex === 0) {
+        nextImgIndex = lastImgIndex;
+      } else {
+        nextImgIndex = currentImgIndex - 1;
+      }
+    }
+    //GET <a> ELEMENT USING NEW INDEX
+    const nextImg = images[nextImgIndex];
+    //UPDATE currentImg
+    currentImg = nextImg;
+    //REMOVE CURRENT CLASS FROM OLD <a>
+    current.removeClass('current');
+    //ADD CURRENT CLASS TO NEW <a>
+    $(nextImg).addClass('current');
+    //UPDATE LIGHTBOX IMG SRC
+    $('.lightbox-img').attr('src', nextImg.href);
   });
-});
+}); //END DOCUMENT READY
